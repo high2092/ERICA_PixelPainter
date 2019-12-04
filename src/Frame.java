7857c5a168ea819d0eb15d750660b891419a2e47
hhyuw <high2092@gmail.com>
2019-12-04 15:59:43 +0900

    create Frame

---
+++ src/Frame.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Frame extends JFrame{
+    public Frame(){
+		this.setSize(new Dimension(400, 400));
+        this.setVisible(true);
+    }
+}
