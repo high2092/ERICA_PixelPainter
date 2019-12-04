0fa3e5408350090a0b3033e76aaf813ee0944deb
hhyuw <high2092@gmail.com>
2019-12-04 16:15:58 +0900

    Canvas

--- src/Frame.java
+++ src/Frame.java
@@ -2,9 +2,14 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Frame extends JFrame{
+
+    Canvas canvas = new Canvas();
     public Frame(){
 		this.setSize(new Dimension(400, 400));
         this.setVisible(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        this.setTitle("PixelPainter");
+        this.add(canvas);
     }
 }
