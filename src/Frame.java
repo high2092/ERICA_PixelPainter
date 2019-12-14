2ae65c0bc436ef83c3065f2321d75e08736bf744
hhyuw <high2092@gmail.com>
2019-12-14 16:01:07 +0900

    Menubar

--- src/Frame.java
+++ src/Frame.java
@@ -1,33 +1,46 @@
-import java.awt.*;
-import javax.swing.*;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import java.awt.Color; // Color.white
+import java.awt.Dimension; // Dimension(int, int)
 
 public class Frame extends JFrame{
 
     Canvas canvas = new Canvas();
     JPanel bg = new JPanel();
-    Dimension sizeof_frame = new Dimension(400, 400);
+    JMenuBar menubar = new JMenuBar();
+
+    Dimension sizeof_frame = new Dimension(400, 360);
+    Dimension sizeof_canvas = new Dimension(310, 310);
+
     public Frame(){
 		this.setSize(sizeof_frame);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        bg.setBackground(Color.black);
-        
+        getContentPane().setBackground(Color.black);
         this.setTitle("PixelPainter");
 
 		this.setLayout(null);
 
 
-        canvas.setSize(new Dimension(200, 200));
-        canvas.setLocation(50, 50);
+        canvas.setSize(sizeof_canvas);
+        canvas.setLocation(0, 50);
         canvas.setVisible(true);
         
 
         bg.setSize(sizeof_frame);
         bg.setVisible(true);
 
-        this.add(canvas); // low priority
-        this.add(bg); // high priority
+        this.add(canvas);
 
         canvas.setBackground(Color.white);
+
+        setJMenuBar(menubar);
+        JMenu file = new JMenu("File");
+        menubar.add(file);
     }
 }
