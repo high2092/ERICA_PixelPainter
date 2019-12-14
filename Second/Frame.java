dfe73712647abe0fd10fc8edfaed4988f9516aa3
hhyuw <high2092@gmail.com>
2019-12-14 21:13:25 +0900

    second

--- Second/Frame.java
+++ Second/Frame.java
@@ -4,14 +4,22 @@ import java.awt.Color;
 
 
 public class Frame extends JFrame{
-    Dimension dim_f = new Dimension(400, 400);
+    Dimension dim_f = new Dimension(450, 400);
+    Dimension dim_c = new Dimension(350, 350);
+
+    Canvas canvas = new Canvas();
+
     public Frame(){
+        getContentPane().setBackground(Color.BLACK);
         setLocation(400, 400);
         setTitle("PixelPainter");
         setSize(dim_f);
         setLayout(null);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().setBackground(Color.BLACK);
+
+        add(canvas);
+        canvas.setSize(dim_c);
+        canvas.setLocation(0, 50);
     }
 }
