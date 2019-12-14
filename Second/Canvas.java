6c625dcc76eb12a160f3912ebea953cc97e3f750
hhyuw <high2092@gmail.com>
2019-12-14 21:30:11 +0900

    second

--- Second/Canvas.java
+++ Second/Canvas.java
@@ -1,6 +1,66 @@
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.border.*;
 
 public class Canvas extends JPanel{
+    int size = 40;
+    Dimension dim_c = new Dimension(350, 350);
+    
+    public Canvas(){
+        setSize(dim_c);
+        setLocation(0, 50);
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                gbc.gridx = col;
+                gbc.gridy = row;
+
+                CellPane cellPane = new CellPane();
+                Border border = null;
+                if (row < size-1) {
+                    if (col < size-1) {
+                        border = new MatteBorder(1, 1, 0, 0, Color.BLACK);
+                    } else {
+                        border = new MatteBorder(1, 1, 0, 1, Color.BLACK);
+                    }
+                } else {
+                    if (col < size-1) {
+                        border = new MatteBorder(1, 1, 1, 0, Color.BLACK);
+                    } else {
+                        border = new MatteBorder(1, 1, 1, 1, Color.BLACK);
+                    }
+                }
+                cellPane.setBorder(border);
+                add(cellPane, gbc);
+            }
+        }
+    }
+}
+
+class CellPane extends JPanel {
+
+    private Color defaultBackground;
+
+    public CellPane() {
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                defaultBackground = getBackground();
+                setBackground(Color.black);
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                setBackground(Color.black);
+                defaultBackground = getBackground();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setBackground(defaultBackground);
+            }
+        });
+    }
 }
--- Second/Frame.java
+++ Second/Frame.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 
 public class Frame extends JFrame{
     Dimension dim_f = new Dimension(450, 400);
-    Dimension dim_c = new Dimension(350, 350);
 
     Canvas canvas = new Canvas();
 
@@ -14,12 +13,10 @@ public class Frame extends JFrame{
         setLocation(400, 400);
         setTitle("PixelPainter");
         setSize(dim_f);
-        setLayout(null);
+        // setLayout(null);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         add(canvas);
-        canvas.setSize(dim_c);
-        canvas.setLocation(0, 50);
     }
 }
